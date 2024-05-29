030fa0adefb7d1247923e1066c32d0ff4cea1b9f
JohnIacutoneP <icorpjohn@gmail.com>
2024-05-29 11:13:59 +1000

    more

--- octopus/variables.tf
+++ octopus/variables.tf
@@ -1,8 +1,8 @@
 variable "OCTOPUS_API_KEY" {
   type        = string
   description = "Octopus Api Key"
-#  default = "API-90L5KKSWXWMGNYG0WZ7GA1IBKZI6XSU"
-  sensitive   = true
+  #  default = "API-90L5KKSWXWMGNYG0WZ7GA1IBKZI6XSU"
+  sensitive = true
 }
 
 variable "OCTOPUS_URL" {
